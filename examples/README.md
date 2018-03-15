f275e0fea8dc6ebeef1409235a912b60fcde49c6
saurabhvaj <saurabhvaj>
2018-03-16 02:38:29 +0530

    added remote-command-executor.py to run command or scripts on remote using python

---
+++ examples/README.md
@@ -0,0 +1,31 @@
+# Remote Command Executor
+
+### Introduction:
+With my learnings on python I came to a situation where I need to run some adhock command on the server, to get relevant information quickly without any special arrangement.  
+One of the possible solution is **Ansible**, to be run in a adhock manner.But I need something more simple than installation and configuration of ansible on my server.  
+Thus I look for scripts, and with python I found a simple solution [Here](https://gist.github.com/batok/2352501).  
+I have updated script to be more user interactive.
+
+
+### Configuration:
+* Prerequisite
+	* Python3
+	* paramiko
+		* Install paramiko using `pip install paramiko` 
+
+
+
+* Setup:
+    *  Create alias using `alias runc='python /path/to/remote-command-executor.py'`
+    *  To run   
+    **`runc -H localhost  -i ~/.ssh/id_rsa -s 'lsb_release -a'`**
+    
+        OR
+    
+    * Run directly as   
+    **`python3 remote-command-executor.py -H localhost  -i ~/.ssh/id_rsa -s 'free -m'`**  
+    
+
+
+
+
---
+++ examples/remote-command-executor.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+# pip install paramiko
+# check for help with python paramiko_example.py --help
+# python paramiko_example.py  /home/delhivery/.ssh/done-key.pem  52.70.172.225 ubuntu 'sudo netstat -tunlp'
+# Or crate a alias first
+# alias runc='python /path/to/paramiko_example.py'
+# 
+
+
+import paramiko
+import optparse
+import getpass
+import time
+
+parser = optparse.OptionParser()
+
+parser.add_option('-i', '--identity-key', action="store", dest="key",help="SSH key for access", default='/home/' + getpass.getuser() + '/.ssh/id_rsa')
+parser.add_option('-H', '--host', action="store", dest="host",help="Target hostname like localhost", default="localhost")
+parser.add_option('-u', '--user', action="store", dest="user",help="Target username like ubuntu", default="ubuntu")
+parser.add_option('-s', '--script', action="store", dest="script",help="Script to run on target server.", default="ls -al")
+
+options, args = parser.parse_args()
+
+keypath = options.key
+hostname = options.host
+username = options.user
+scripttorun = options.script 
+
+
+
+k = paramiko.RSAKey.from_private_key_file(keypath)
+c = paramiko.SSHClient()
+c.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+print("Connecting to " + hostname)
+print(".........                          (33%)\r")
+time.sleep(1)
+print("......................             (66%)\r")
+time.sleep(1)
+print("...................................(100%)\r")
+c.connect( hostname = hostname, username = username, pkey = k )
+print("Connected :) ")
+print("")
+commands = [ scripttorun ]
+for command in commands:
+	print("Executing: {}".format( command ))
+	stdin , stdout, stderr = c.exec_command(command)
+	print(stdout.read().decode())
+	print( "Errors")
+	print(stderr.read())
+c.close()
+
